a6580b96e72d7c69f13fa66119eef2ec2e7ee2fd
ValeriiaFediuk <fediuk.valeriia14@gmail.com>
2024-08-14 21:33:28 +0200

    second trial

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
@@ -20,11 +19,25 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-          entryFileNames: 'commonHelpers.js',
+          entryFileNames: '[name]-[hash].js', // Виправлено формат імені файлів для унікальності
         },
       },
       outDir: '../dist',
+      emptyOutDir: true, // Очищення директорії перед збиранням
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+    ],
+    server: {
+      fs: {
+        allow: [
+          // Дозволяємо доступ до директорії проекту
+          '/Users/mac/Desktop/projects/goit-advancedjs-hw-04',
+          // Додаємо шлях до node_modules
+          '/Users/mac/Desktop/projects/goit-advancedjs-hw-04/node_modules',
+        ],
+      },
+    },
   };
 });
